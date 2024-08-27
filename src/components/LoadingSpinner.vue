134d549e7708d38dc21bd0a4b4263731a39203c4
Farhain Humaira <140294687+Farhain-Humaira@users.noreply.github.com>
2024-08-27 13:59:43 +0800

    Add files via upload

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="loading-spinner">
+      <div class="spinner-border" role="status"></div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'LoadingSpinner',
+  };
+  </script>
+  
+  <style scoped>
+  .loader {
+  border: 5px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 5px solid #3498db;
+  width: 50px;
+  height: 50px;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+  </style>
+  
