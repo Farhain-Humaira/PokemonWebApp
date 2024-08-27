8f3651f52e5c33f37ad1c7d65f37d9cd024e38e2
Farhain Humaira <140294687+Farhain-Humaira@users.noreply.github.com>
2024-08-27 13:58:20 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <section class="welcome-section">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <h3>Welcome, Trainer!</h3>
+      <h6>You’ve just entered the Pokémon world! <br>Get ready to catch, explore, and discover amazing Pokémon.</h6>
+    </section>
+  <div id="app">
+    <PokemonList />
+  </div>
+</template>
+
+<script>
+  import PokemonList from './components/PokemonList.vue';
+
+export default {
+  components: {
+    PokemonList
+  }
+};
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+section {
+  color: white;
+  background-color: #2c3e50;
+  padding: 10px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createPinia } from 'pinia';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './assets/style.css';
+
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia);
+app.mount('#app');
