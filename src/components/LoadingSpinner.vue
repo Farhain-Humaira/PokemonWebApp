e683f1c5ce7459bc8224fe6c25562b76cd71534c
Farhain-Humaira <humairafarhain@gmail.com>
2024-08-27 22:43:36 +0800

    Update LoadingSpinner.vue

--- src/components/LoadingSpinner.vue
+++ src/components/LoadingSpinner.vue
@@ -1,20 +1,29 @@
 <template>
-  <div class="loader" role="status"></div>
+  <div class="loader-wrapper">
+    <div class="loader" role="status"></div>
+  </div>
 </template>
-  
-  <script>
-  export default {
-    name: 'LoadingSpinner',
-  };
-  </script>
-  
-  <style scoped>
-  .loader {
+
+<script>
+export default {
+  name: 'LoadingSpinner',
+};
+</script>
+
+<style scoped>
+.loader-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh; 
+}
+
+.loader {
   border: 5px solid #000000;
   border-radius: 50%;
-  border-top: 5px solid #2e4352;
-  width: 50px;
-  height: 50px;
+  border-top: 5px solid #138adf;
+  width: 25px;
+  height: 25px;
   animation: spin 1s linear infinite;
 }
 
@@ -22,5 +31,4 @@
   0% { transform: rotate(0deg); }
   100% { transform: rotate(360deg); }
 }
-  </style>
-  
+</style>
